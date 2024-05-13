d4ad57fbddb308539267def7497e8532e4934755
Okegbe Akpofure Kelvin-faith <okegbeakpofurekelvin@gmail.com>
2024-05-13 18:36:19 +0100

    setting up project sructure

---
+++ src/constants/fonts.tsx
@@ -0,0 +1,12 @@
+const fontSizes = {
+  xs: "0.4em",
+  s: "0.5em",
+  m: "0.75em",
+  l: "1em",
+  xl: "1.25em",
+  xxl: "1.5em",
+  xxxl: "1.75em",
+  xxxxl: "2em",
+};
+
+export default fontSizes;
